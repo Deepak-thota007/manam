6897800ddd2ca570a0e8d46bb628437e4fb4e08c
Deepak-thota007 <mailtodeepu2016@gmail.com>
2024-02-23 22:55:27 +0530

    Hospital data added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import HospitalData from './Jsondata/Hospital.json';
+
 
 const About = () => {
   return (
-    <div>
-     <h1 className='text-primary'>NO Medicine for Poor</h1>
-     </div>
-  )
+    <div className="container">
+      <h1 className="text-primary text-center">Hospitals</h1>
+
+      <div className="row">
+        {HospitalData.data.map((item) => (
+          <div
+            key={item.id}
+            className="col-12 col-md-6 col-lg-4 mb-3"
+          >
+            <div
+              className={`card bg-${item.color}`}
+              style={{
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                height: '200px',
+                borderRadius: '10px',
+                textAlign:'center',
+                color:''
+              }}
+            ><b>{item.name}</b>
+            <br/>
+            <br/>
+            Doctor:{item.doctor}
+            <br/>
+            Address:{item.address}
+            <br/>
+            phno: {item.phno}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default About
