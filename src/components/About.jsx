74499ff42d8f9c7fc736c16a13ea11292c8b3759
Deepak-thota007 <mailtodeepu2016@gmail.com>
2024-02-26 12:01:34 +0530

    water component updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HospitalData from './Jsondata/Hospital.json';
-
+import Medical from './Jsondata/Medical.json';
 
 const About = () => {
   return (
@@ -9,6 +9,36 @@ const About = () => {
 
       <div className="row">
         {HospitalData.data.map((item) => (
+          <div
+            key={item.id}
+            className="col-12 col-md-6 col-lg-4 mb-3"
+          >
+            <div
+              className={`card bg-${item.color}`}
+              style={{
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                height: '200px',
+                borderRadius: '10px',
+                textAlign:'center',
+                color:''
+              }}
+            ><b>{item.name}</b>
+            <br/>
+            <br/>
+            Doctor:{item.doctor}
+            <br/>
+            Address:{item.address}
+            <br/>
+            phno: {item.phno}
+            </div>
+          </div>
+        ))}
+      </div>
+      <h1 className="text-primary text-center" >Medical shops</h1>
+      <div className="row">
+        {Medical.data.map((item) => (
           <div
             key={item.id}
             className="col-12 col-md-6 col-lg-4 mb-3"
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,46 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1 className='text-primary'>Pure salt water..</h1>
-      
+      <h1 className='text-primary text-center'>Tap water timings</h1>
+      <div className="row justify-content-center">
+    <div className="col-12 col-md-1 col-lg-6 mb-3 text-center">
+    <div className="card text-center">
+  <div className="card-header">
+    <b>Ambedhkar Center</b>
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+  <div className="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+    </div>
+    <div className="col-12 col-md-1 col-lg-6 mb-3 text-center">
+    <div className="card text-center">
+  <div className="card-header">
+   <b> High School Centre</b>
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+  <div className="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+    </div>
+
+      </div>
+      <h1 className='text-primary text-center'>Drinking Water Timings & Details</h1>
+      <p className='text-center'>Timings:- 24/7 Available</p>
+      <pre className='text-center'>Details:-<br/>
+        Each can:- 7 Rs (20 L)<br/>
+        Monthly:- 210 Rs (30 Swipes)
+      </pre>
     </div>
   )
 }
