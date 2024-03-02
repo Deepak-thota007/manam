0549f08cef93254cf83839592caed7b1b75cdd2d
Deepak-thota007 <mailtodeepu2016@gmail.com>
2024-03-02 09:49:37 +0530

    nav  page upsates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './cssFolder/nav.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg  ">
       <div className="container">
-        <Link className="navbar-brand" to="/">Our Arts 😊</Link>
+        <Link className="navbar-brand" to="/"><b>Our Arts 😊</b></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
+        <div className="collapse navbar-collapse" id="navbarNav"
+        style={{
+          background:'white',
+          width:'360px',
+          justifyContent:'space-between',
+          display:'flex',
+          padding:'5px',
+          borderRadius:'22px',
+          boxShadow:'2px 2px 20px rgba(0,0,0,0.3)'
+        }}
+        >
+          <ul className="navbar-nav ml-auto" >
+            
           <li className="nav-item">
-              <Link className="nav-link" to="/manam/admin">🦁admin</Link>
+              <Link className="nav-link " to="/manam/admin">Admin</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/manam/base">📚 విద్య</Link>
+              <Link className="nav-link" to="/manam/base"> Education</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/manam/about">🏢 వైద్యం</Link>
+              <Link className="nav-link" to="/manam/about">Medical</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/manam/contact">💦 నీరు</Link>
+              <Link className="nav-link" to="/manam/contact"> Water</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/manam/problem">🤦‍♂️ సమస్యలు</Link>
+              <Link className="nav-link" to="/manam/problem">Problems</Link>
             </li>
             <li className="nav-item"> 
-              <Link className="nav-link" to="/manam/polacy">📝 పఢకాలు</Link>
+              <Link className="nav-link" to="/manam/polacy">Polacies</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/manam/seva">📜 సేవ</Link>
+              <Link className="nav-link" to="/manam/seva"> Service</Link>
             </li>
           </ul>
         </div>
